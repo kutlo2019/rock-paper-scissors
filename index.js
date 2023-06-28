41bce581edd8a4aeb9dc482728f8b1097bb1a069
kutlo2019 <kutlopopo@rocketmail.com>
2023-06-28 15:03:09 +0200

    create buttons and add event listeners

--- index.js
+++ index.js
@@ -53,6 +53,29 @@ function playRound(userSelection, computerSelection) {
     }
 }
 
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', e => {
+        const userSelection = e.target.id;
+        const computerSelection = getComputerChoice();
+        const winner = playRound(userSelection, computerSelection);
+        const result = document.createElement('h2');
+        if (winner === 'None') {
+            result.innerHTML = "Result is a Draw";
+        } else {
+            result.innerHTML = `The winner is the ${winner}!`;
+        }
+        const screen = document.querySelector('.screen');
+        console.log(screen.children[0])
+        if (screen.children[0] === undefined) {
+            screen.appendChild(result);
+        } else {
+            screen.replaceChild(result, screen.childNodes[0]);
+        }
+    });
+});
+
 function game() {
     let userRounds = 0;
     let compRounds = 0;
